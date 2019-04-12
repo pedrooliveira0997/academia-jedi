8e795a66abde5e105ca3806f5ab79b3a43fb82a0
Glaydson Vasconcelos de Sousa <glaydson.sousa@bcb.gov.br>
2019-04-12 17:04:01 -0300

    Alterações após exercício 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaEstudantesComponent } from './estudantes/lista-estudantes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaEstudantesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
